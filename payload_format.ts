b380d91fb41e406a2c5faf11659395639dc756e0
cbuchost <christopher.buchmann@ost.ch>
2025-01-21 09:26:17 +0100

    improve number payload

--- payload_format.ts
+++ payload_format.ts
@@ -20,17 +20,31 @@ namespace IoTCube {
     let payloadFormatter = new PayloadFormatter()
 
     /**
- * Send your added Data over LoRa network.
- * @param chaNum is the LoRa channel used during transmit
-*/
+     * Send your added Data over LoRa network.
+     * @param chaNum is the LoRa channel used during transmit
+    */
     //% blockId="LoRa_Send_Payload"
     //% block="Send added Data on f-Port %fport"
     //% subcategory="PayloadFormatter" group="Send"
     //% fport.min=1
     //% fport.max=222
     //% fport.defl=1
-    export function SendPayload(fport?: number,) {
+    export function SendPayload(fport?: number) {
         writeATCommand("SEND", fport + ":" + Buffer.fromArray(payloadFormatter.payload).toHex())
+    }
+
+        /**
+     * Send your added Data over LoRa network.
+     * @param chaNum is the LoRa channel used during transmit
+    */
+    //% blockId="LoRa_Clear_Payload"
+    //% block="Clear Payload
+    //% subcategory="PayloadFormatter" group="Send"
+    //% fport.min=1
+    //% fport.max=222
+    //% fport.defl=1
+    export function ClearPayload() {
+        payloadFormatter.payload = []
     }
 
     /**
@@ -40,8 +54,23 @@ namespace IoTCube {
     //% block="Add Number Input %data on Channel %channel"
     //% subcategory="PayloadFormatter" 
     //% group="Senden"
-    export function addNumberInput(data: number) {
-        payloadFormatter.payload.push(payloadTypes.Integer)
-        payloadFormatter.payload.push(data)
+    export function encodeSignedInteger(data: number) {
+        if (data < -0x80000000 || data > 0x7fffffff) {
+                //throw new Error("Number out of range for signed 4 bytes");
+
+            }
+
+            payloadFormatter.payload.push(payloadTypes.Integer); // Type marker for signed integer
+
+        if (data < 0) {
+            data = 0x100000000 + data; // Convert negative to two's complement
+            }
+
+        // 0xff is for 255
+        // & is a bitwise AND
+        payloadFormatter.payload.push((data >> 24) & 0xff); // Most significant byte
+        payloadFormatter.payload.push((data >> 16) & 0xff);
+        payloadFormatter.payload.push((data >> 8) & 0xff);
+        payloadFormatter.payload.push(data & 0xff);         // Least significant byte
+        }
     }
-}
